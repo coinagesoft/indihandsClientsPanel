6c6513c97a62ca9303cbcbfd509a305c5bdb894a
Pradnya129 <pradnyamandekar129@gmail.com>
2026-02-24 17:48:11 +0530

    favicon

--- app/db.js
+++ app/db.js
@@ -4,25 +4,25 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// export const db = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0
-// });
-
 export const db = mysql.createPool({
-  host: "217.21.74.127",
-  user: "u856729253_indihandsport",
-  password: "oPSOg3#a2P>",
-  database: "u856729253_Indihands",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
+
+// export const db = mysql.createPool({
+//   host: "217.21.74.127",
+//   user: "u856729253_indihandsport",
+//   password: "oPSOg3#a2P>",
+//   database: "u856729253_Indihands",
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
 
 // Test connection
 (async () => {
