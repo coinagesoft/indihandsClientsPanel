4112f277f7d3891b861c91892ee4585b981cb09e
Pradnya129 <pradnyamandekar129@gmail.com>
2026-03-02 16:19:07 +0530

    terms

--- app/client/terms/page.js
+++ app/client/terms/page.js
@@ -1,9 +1,6 @@
-"use client";
 import React from 'react'
-import useAuthGuard from '../hooks/useAuthGuard';
 
 const page = () => {
-     useAuthGuard();
   return (
     <div className='text-center mt-t pt-5 pageTitle'>Terms and conditions</div>
   )
