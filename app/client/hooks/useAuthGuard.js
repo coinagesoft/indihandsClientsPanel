ba24abfc32fa20d0fe26f675b006fd86482223f3
Pradnya129 <pradnyamandekar129@gmail.com>
2026-03-11 16:38:24 +0530

    auth

--- app/client/hooks/useAuthGuard.js
+++ app/client/hooks/useAuthGuard.js
@@ -1,3 +1,20 @@
+// "use client";
+// import { useEffect } from "react";
+// import { useRouter } from "next/navigation";
+
+// export default function useAuthGuard() {
+//   const router = useRouter();
+
+//   useEffect(() => {
+//     const token = localStorage.getItem("client_token");
+
+//     if (!token) {
+//       router.replace("/login");
+//     }
+//   }, [router]);
+// }
+
+
 "use client";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
@@ -10,6 +27,21 @@ export default function useAuthGuard() {
 
     if (!token) {
       router.replace("/login");
+      return;
     }
+
+    try {
+      const payload = JSON.parse(atob(token.split(".")[1]));
+      const isExpired = payload.exp * 1000 < Date.now();
+
+      if (isExpired) {
+        localStorage.removeItem("client_token");
+        router.replace("/login");
+      }
+    } catch (error) {
+      localStorage.removeItem("client_token");
+      router.replace("/login");
+    }
+
   }, [router]);
 }
