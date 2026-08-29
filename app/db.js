4d2adf4e448f3340053c9f16b3c7a3a91484e18e
Coinage@1790 <pradnyamandekar129@gmail.com>
2026-08-29 21:51:59 +0530

    storage url1

--- app/db.js
+++ app/db.js
@@ -5,15 +5,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 
-export const db = mysql.createPool({
-  host: "localhost",   // 🔥 IMPORTANT (not live server)
-  user: "root",
-  password: "NewPassword123!", // or "" if none
-  database: "indihand__test_local_backup",    
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+// export const db = mysql.createPool({
+//   host: "localhost",   // 🔥 IMPORTANT (not live server)
+//   user: "root",
+//   password: "NewPassword123!", // or "" if none
+//   database: "indihand__test_local_backup",    
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
 
 // export const db = mysql.createPool({
 //   host: process.env.DB_HOST,
@@ -37,14 +37,14 @@ export const db = mysql.createPool({
 // })
 
 
-// export const db = mysql.createPool({
-//   host: "46.28.45.124",
-//   user: "u144125849_Indicat",
-//   password: "wozvis-fipnez-kyvCe4",
-//   database: "u144125849_catalog",
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0
-// });
+export const db = mysql.createPool({
+  host: "46.28.45.124",
+  user: "u144125849_Indicat",
+  password: "wozvis-fipnez-kyvCe4",
+  database: "u144125849_catalog",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
 
 export default db;
